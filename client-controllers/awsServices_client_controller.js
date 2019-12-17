b1dd61bf1342c97999f08da4849a81093eb7c814
Ashish Kathuria <xs2ashish@gmail.com>
2019-12-17 23:06:07 +0000

    updated client controller

--- client-controllers/awsServices_client_controller.js
+++ client-controllers/awsServices_client_controller.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 	getAwsServicesDetails();
 });
 
-var getAwsServicesDetails = function() {
-	$("#div_body_awsServices").append("ashish");
+function getAwsServicesDetails() {
+	$.get('getCloudList').done(function(result){
+		if (result.length>0) {
+			for (i =0; i<result.length; i++) {
+				$("#div_body_awsServices").append('<tr>' + 
+					'<td>' + result[i].cloud_name + 
+					'</td> </tr>');
+			}
+		}
+
+});
 }
+
